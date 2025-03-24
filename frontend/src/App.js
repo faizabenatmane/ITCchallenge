d955776209ad9707a468e214f82dc6b357c284c9
faiza benatmane <faizabenatmane@gmail.com>
2025-03-24 17:31:55 +0100

    Switch to HashRouter for GitHub Pages deployment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import WelcomePage from './pages/WelcomePage';
 import LoginPage from './pages/LoginPage';
 
